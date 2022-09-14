143320277f2abf0e826be6f2e3fff47ef4b489da
Suchismita Sur <45706690+suchisur@users.noreply.github.com>
2022-09-14 17:52:10 +0530

    Update secgrp.tf

--- secgrp.tf
+++ secgrp.tf
@@ -15,27 +15,6 @@ resource "aws_security_group" "iac-task-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-#Prometheus
-  ingress {
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-#Alertmanager
- ingress {
-    from_port   = 9093
-    to_port     = 9093
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-#Blackbox Exporter
-  ingress {
-    from_port   = 9115
-    to_port     = 9115
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   egress {
         from_port   = 0
     to_port     = 0
